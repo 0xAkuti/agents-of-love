86538336085ff58d151455a13bb9b2e183ab0426
zanastas <83847982+zanastas@users.noreply.github.com>
2025-02-06 10:49:34 +0700

    add script to validate agents

---
+++ check_agent.py
@@ -0,0 +1,9 @@
+from model import Agent
+import sys
+
+with open(sys.argv[1], "r") as f:
+    try:
+        agent = Agent.model_validate_json(f.read())
+        print('Agent is valid')
+    except Exception as e:
+        print(e)
