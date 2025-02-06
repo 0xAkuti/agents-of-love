8825c02ef059665d973448b4315f1020e04e3dfa
Akuti <40723201+0xAkuti@users.noreply.github.com>
2025-02-06 17:37:20 +0700

    tidy: remove the wrapper

--- src/agent_with_wallet.py
+++ src/agent_with_wallet.py
@@ -5,14 +5,12 @@ from cdp_langchain.agent_toolkits import CdpToolkit
 from src.cdp_landchain_adapter import CDPLangChainToolAdapter
 from cdp_langchain.utils.cdp_agentkit_wrapper import CdpAgentkitWrapper
 import dotenv
-import functools
 from autogen_core.models import ChatCompletionClient
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 dotenv.load_dotenv(override=True)
 
 class AgentWithWallet(AssistantAgent):
-    @functools.wraps(AssistantAgent.__init__)
     def __init__(self, name: str, system_message: str, model_client: ChatCompletionClient, **kwargs):
         self.cdp_agentkit = CdpAgentkitWrapper(
             network_id=os.environ.get("NETWORK_ID") # default to base-sepolia
