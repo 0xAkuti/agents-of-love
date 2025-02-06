f66614c08ca43a7adefc01b9463f7174533939dd
Akuti <40723201+0xAkuti@users.noreply.github.com>
2025-02-06 10:30:00 +0700

    impl: make model load and save from json

--- model.py
+++ model.py
@@ -1,19 +1,29 @@
 from typing import Optional
 from pydantic import BaseModel
 import enum
+import pathlib
+class StoreableBaseModel(BaseModel):
+    def save(self, path: str | pathlib.Path):
+        with open(path, 'w') as file:
+            file.write(self.model_dump_json())
+    
+    @classmethod
+    def load(cls, path: str | pathlib.Path):
+        with open(path, 'r') as file:
+            return cls.model_validate_json(file.read())
 
-class MessageContent(BaseModel):
+class MessageContent(StoreableBaseModel):
     text: str
 
-class Message(BaseModel):
+class Message(StoreableBaseModel):
     user: str
     content: MessageContent
 
-class ModelProvider(BaseModel):
+class ModelProvider(StoreableBaseModel):
     provider: str
     model: str
 
-class Character(BaseModel):
+class Character(StoreableBaseModel):
     bio: list[str]
     lore: list[str]
     knowledge: list[str]
@@ -31,7 +41,7 @@ class AgentRole(str, enum.Enum):
     SPEAKER_SELECTOR = "speaker_selector"
     
     
-class Agent(BaseModel):
+class Agent(StoreableBaseModel):
     name: str
     model_provider: ModelProvider
     role: AgentRole
